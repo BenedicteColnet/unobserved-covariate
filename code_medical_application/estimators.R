615f7740d3392b922ba5d41dc655739489856dde
bcolnet <benedicte.colnet@gmail.com>
2021-09-28 16:36:11 +0200

    update in medical analysis

--- code_medical_application/estimators.R
+++ code_medical_application/estimators.R
@@ -1,29 +1,125 @@
-compute_gformula_with_Random_forest <- function(data, verbose = FALSE, tuning = "all"){
-  X <- data[, !names(data) %in% c("S", "A", "Y")]
-  X.m = model.matrix(~.-1, data = X)
+compute_mean_diff_RCT <- function(DF){
+  RCT_ATE <- mean(DF[DF$A == 1 & DF$S == 1, "Y"]) - mean(DF[DF$A == 0  & DF$S == 1, "Y"])  
+  return(RCT_ATE)
+}
+
+compute_ipsw <- function(DF, normalized = FALSE, estimation = "logit"){
   
-  idx_0 <- which(data$S == 1 & data$A == 0)
-  mu_0 = regression_forest(X.m[idx_0, ], data[idx_0, "Y"], tune.parameters = tuning)
+  N <- nrow(DF)
+  n <- nrow(DF[DF$S ==1, ])
+  m <- nrow(DF[DF$S ==0, ])
   
-  if (verbose) {
-    print(paste0("R2 for non treated : ", sum((data[data$S == 1 & data$A == 0, "Y"] - mu_0$predictions)^2) / sum((data[data$S == 1 & data$A == 0, "Y"] - mean(data[data$S == 1 & data$A == 0, "Y"]))^2) ))
+  temp <- DF
+  
+  # Estimation of P(V = 1 | X)
+  # p <-- P(V = 1 | X) 
+  
+  if (estimation == "logit"){
+    
+    # with logistic regression
+    p.fit  <- glm(S ~., family = binomial("logit"), data = temp[, !names(temp) %in% c("A", "Y")])
+    p <- predict(p.fit, type = "response", newdata = temp)
+    
+  } 
+  else if (estimation == "forest") {
+    break
+  } 
+  else {
+    print("Estimation parameter should be forest or logit.")
+    break
   }
   
-  idx_1 <- which(data$S == 1 & data$A == 1)
-  mu_1 = regression_forest(X.m[idx_1, ], data[idx_1, "Y"], tune.parameters = tuning)
+  # Store odds
+  temp$odds <- ((1 - p)/p)
   
-  if (verbose) {
-    print(paste0("R2 for treated : ", sum((data[data$S == 1 & data$A == 1, "Y"] - mu_1$predictions)^2) / sum((data[data$S == 1 & data$A == 1, "Y"] - mean(data[data$S == 1 & data$A == 1, "Y"]))^2) ))
+  # Keep only RCT for the rest of the calculus
+  temp <- temp[temp$S == 1,]
+  
+  if (normalized == FALSE){
+    tau_ipsw <- (2/m)*with(temp, sum(odds*A*Y - odds*(1-A)*Y))  
+  } else {
+    tau_ipsw <- with(temp, sum(odds*A*Y/sum(odds*A) - odds*(1-A)*Y/sum(odds*(1-A))))
   }
   
-  mu_1_hat <- predict(mu_1, newdata = data[data$S==0, !names(data) %in% c("S", "A", "Y")])$predictions
-  mu_0_hat <- predict(mu_0, newdata = data[data$S==0, !names(data) %in% c("S", "A", "Y")])$predictions
+  return(tau_ipsw)
+}
+
+# G-formula
+compute_gformula <- function(DF, continuous_Y = TRUE){
   
-  return(mean(mu_1_hat - mu_0_hat))
+  temp <- DF
+  
+  if(continuous_Y){
+    mu_1 <- lm(Y ~., data = temp[temp$S == 1 & temp$A == 1, !names(temp) %in% c("S", "A")])
+    mu_0 <- lm(Y ~., data = temp[temp$S == 1 & temp$A == 0, !names(temp) %in% c("S", "A")])
+    
+    mu_1_predict <- predict.lm(mu_1, newdata = temp[temp$S == 0, !names(temp) %in% c("S", "A")])
+    mu_0_predict <- predict.lm(mu_0, newdata = temp[temp$S == 0, !names(temp) %in% c("S", "A")])
+    
+    tau_hat_gformula <- mean(mu_1_predict) - mean(mu_0_predict)
+    
+  } else 
+    # binary outcome
+    {
+    mu_1 <- glm(Y ~., family = binomial("logit"), data = temp[temp$S == 1 & temp$A == 1, !names(temp) %in% c("S", "A")])
+    mu_0 <- glm(Y ~., family = binomial("logit"), data = temp[temp$S == 1 & temp$A == 0, !names(temp) %in% c("S", "A")])
+    
+    mu_1_predict <- predict(mu_1, type = "response", newdata = temp[temp$S == 0, !names(temp) %in% c("S", "A")])
+    mu_0_predict <- predict(mu_0, type = "response", newdata = temp[temp$S == 0, !names(temp) %in% c("S", "A")])
+    
+    tau_hat_gformula <- mean(mu_1_predict) - mean(mu_0_predict)
+  }
+  
+  return(tau_hat_gformula)  
 }
 
 
-get_coefficients_with_Robinson_proc <- function(data, learning_m = "forest", covariate_names = COVARIATE_NAMES, ratio = 0.5){
+# AIPSW
+compute_aipsw <- function(DF, normalized = FALSE) {
+  
+  N <- nrow(DF)
+  n <- nrow(DF[DF$S ==1, ])
+  m <- nrow(DF[DF$S ==0, ])
+  
+  temp <- DF
+  
+  ### Outcome modeling
+  mu_1 <- lm(Y ~., data = temp[temp$S == 1 & temp$A == 1, !names(temp) %in% c("S", "A")])
+  mu_0 <- lm(Y ~., data = temp[temp$S == 1 & temp$A == 0, !names(temp) %in% c("S", "A")])
+  
+  # RWE estimation
+  mu_1_predict <- predict.lm(mu_1, newdata = temp[temp$S == 0, !names(temp) %in% c("S", "A")])
+  mu_0_predict <- predict.lm(mu_0, newdata = temp[temp$S == 0, !names(temp) %in% c("S", "A")])
+  tau_gformula <- mean(mu_1_predict) - mean(mu_0_predict)
+  
+  ### IPSW part
+  # Estimation of P(V = 1 | X)
+  # p <-- P(V = 1 | X) 
+  # with logistic regression
+  p.fit  <- glm(S ~., family = binomial("logit"), data = temp[, !names(temp) %in% c("A", "Y")])
+  p <- predict(p.fit, type = "response", newdata = temp)
+  
+  # Store odds
+  temp$odds <- ((1 - p)/p)
+  
+  #keep only rct
+  temp <- temp[temp$S == 1,]
+  
+  temp$mu_11 <- predict.lm(mu_1, newdata = temp[, !names(temp) %in% c("S", "A")])
+  temp$mu_10 <- predict.lm(mu_0, newdata = temp[, !names(temp) %in% c("S", "A")])
+  
+  if (normalized == FALSE){
+    tau_ipsw <- (2/m)*with(temp, sum(odds*A*(Y - mu_11) - odds*(1-A)*(Y - mu_10)))  
+  } else {
+    tau_ipsw <- with(temp, sum(odds*A*(Y - mu_11)/sum(odds*A) - odds*(1-A)*(Y - mu_10)/sum(odds*(1-A))))
+  }
+  
+  tau_aipsw <- tau_ipsw + tau_gformula
+  return(tau_aipsw)
+}
+
+
+get_coefficients_with_Robinson_proc <- function(data, learning_m = "linear", covariate_names = COVARIATE_NAMES, ratio = 0.5){
   
   # focus on RCT only
   temp <- data[data$S == 1, c(covariate_names, "Y", "A")]
@@ -67,33 +163,45 @@ get_coefficients_with_Robinson_proc <- function(data, learning_m = "forest", cov
 }
 
 
-# G-formula
-compute_gformula <- function(DF, continuous_Y = TRUE){
+compute_gformula_with_Random_forest <- function(data, verbose = FALSE, tuning = "all"){
+  X <- data[, !names(data) %in% c("S", "A", "Y")]
+  X.m = model.matrix(~.-1, data = X)
   
-  temp <- DF
+  idx_0 <- which(data$S == 1 & data$A == 0)
+  mu_0 = regression_forest(X.m[idx_0, ], data[idx_0, "Y"], tune.parameters = tuning)
   
-  if(continuous_Y){
-    mu_1 <- lm(Y ~., data = temp[temp$S == 1 & temp$A == 1, !names(temp) %in% c("S", "A")])
-    mu_0 <- lm(Y ~., data = temp[temp$S == 1 & temp$A == 0, !names(temp) %in% c("S", "A")])
-    
-    mu_1_predict <- predict.lm(mu_1, newdata = temp[temp$S == 0, !names(temp) %in% c("S", "A")])
-    mu_0_predict <- predict.lm(mu_0, newdata = temp[temp$S == 0, !names(temp) %in% c("S", "A")])
-    
-    tau_hat_gformula <- mean(mu_1_predict, na.rm = T) - mean(mu_0_predict, na.rm = T)
-    
-  } else 
-    # binary outcome
-  {
-    mu_1 <- glm(Y ~., family = binomial("logit"), data = temp[temp$S == 1 & temp$A == 1, !names(temp) %in% c("S", "A")])
-    mu_0 <- glm(Y ~., family = binomial("logit"), data = temp[temp$S == 1 & temp$A == 0, !names(temp) %in% c("S", "A")])
-    
-    mu_1_predict <- predict(mu_1, type = "response", newdata = temp[temp$S == 0, !names(temp) %in% c("S", "A")])
-    mu_0_predict <- predict(mu_0, type = "response", newdata = temp[temp$S == 0, !names(temp) %in% c("S", "A")])
-    
-    tau_hat_gformula <- mean(mu_1_predict, na.rm = T) - mean(mu_0_predict, na.rm = T)
+  if (verbose) {
+    print(paste0("R2 for non treated : ", sum((data[data$S == 1 & data$A == 0, "Y"] - mu_0$predictions)^2) / sum((data[data$S == 1 & data$A == 0, "Y"] - mean(data[data$S == 1 & data$A == 0, "Y"]))^2) ))
   }
   
-  return(tau_hat_gformula)  
+  idx_1 <- which(data$S == 1 & data$A == 1)
+  mu_1 = regression_forest(X.m[idx_1, ], data[idx_1, "Y"], tune.parameters = tuning)
+  
+  if (verbose) {
+    print(paste0("R2 for treated : ", sum((data[data$S == 1 & data$A == 1, "Y"] - mu_1$predictions)^2) / sum((data[data$S == 1 & data$A == 1, "Y"] - mean(data[data$S == 1 & data$A == 1, "Y"]))^2) ))
+  }
+  
+  mu_1_hat <- predict(mu_1, newdata = data[data$S==0, !names(data) %in% c("S", "A", "Y")])$predictions
+  mu_0_hat <- predict(mu_0, newdata = data[data$S==0, !names(data) %in% c("S", "A", "Y")])$predictions
+  
+  return(mean(mu_1_hat - mu_0_hat))
 }
 
 
+compute_gformula_with_ranger <- function(data){
+  
+  mu_1 =  ranger(Y ~ ., 
+                 num.trees = 1000,
+                 data = data[data$S == 1 & data$A == 1, !names(data) %in% c("S", "A")],
+                 splitrule = "variance")
+  mu_0 =  ranger(Y ~ ., 
+                 num.trees = 1000,
+                 data = data[data$S == 1 & data$A == 0, !names(data) %in% c("S", "A")],
+                 splitrule = "variance")
+  
+  
+  mu_1_hat <- predict(mu_1, data = data[data$S==0, !names(data) %in% c("S", "A", "Y")])$predictions
+  mu_0_hat <- predict(mu_0, data = data[data$S==0, !names(data) %in% c("S", "A", "Y")])$predictions
+  
+  return(mean(mu_1_hat - mu_0_hat))
+}
